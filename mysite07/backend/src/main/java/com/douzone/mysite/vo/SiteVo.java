7d86865fe2542ec051da3aa38b65c7caa7bb8c4c
Oh-HyunYoung <gusdud2709@gmail.com>
2023-03-22 13:05:21 +0900

    mysite07 #site upload

---
+++ mysite07/backend/src/main/java/com/douzone/mysite/vo/SiteVo.java
@@ -0,0 +1,18 @@
+package com.douzone.mysite.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+
+public class SiteVo {
+	private Long no;
+	private String title;
+	private String welcome;
+	private String profile;
+	private String description;
+	
+}
